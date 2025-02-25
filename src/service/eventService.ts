4c4fcc5e4fb714be87184c3b1ce46feb8312c2a9
MinChatr <min_etc@hotmail.com>
2025-02-25 14:28:05 +0700

    Import * as repo from '../repository/eventRepository' In Case of Having Several Function

--- src/service/eventService.ts
+++ src/service/eventService.ts
@@ -1,25 +1,20 @@
 // สร้าง type ของ object ที่จะใช้ แต่ว่าเรา ย้าย type ไปไว้ที่ eventService.ts เราก็ต้อง import เข้ามา 
 import type { Event } from '../models/event.ts';
 
-import {
-  addEvent as addNewEvent,
-  getAllEvents as allEvents,
-  getEventByCategory as eventByCategory,
-  getEventById as eventById
-} from '../repository/eventRepository.js';
+import * as repo from '../repository/eventRepository.js';
 
 export function getEventByCategory(category: string): Promise<Event[]> {
-  return eventByCategory(category);
+  return repo.getEventByCategory(category);
 }
 
 export function getAllEvents(): Promise<Event[]> {
-  return allEvents();
+  return repo.getAllEvents();
 }
 
 export function getEventById(id: number): Promise<Event | undefined> {
-  return eventById(id);
+  return repo.getEventById(id);
 }
 
 export function addEvent(newEvent: Event): Promise<Event> {
-  return addNewEvent(newEvent);
+  return repo.addEvent(newEvent);
 }
