595994b7c0a76dd0650019f34f043a8ebddc886f
MinChatr <min_etc@hotmail.com>
2025-02-25 09:54:05 +0700

    Using Data Type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 console.log("Hello, TypeScript with Node.js!");
+
+let x : number | string = 10; 
+// x = 'hello'; // x is a string
+x = 5; // x is a number
+
+if (typeof x === 'string'){ 
+  console.log('x is a string'); 
+} else if (typeof x === 'number'){ 
+  console.log('x is a number')
+} else { 
+  console.log('x is neither a string nor a number'); 
+}
+
