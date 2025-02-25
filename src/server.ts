e2fb73ed0c8898beda2943d409533aa692365cf5
MinChatr <min_etc@hotmail.com>
2025-02-25 12:53:40 +0700

    Create Endpoint for Add Data (Post Method)

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express'
 const app = express()
 const port = 3000
+app.use(express.json())
 
 app.get('/', (req : Request, res : Response) => { 
   res.send('Hello World')
@@ -48,6 +49,13 @@ app.get("/events/:id", (req, res) => {
   res.status(404).send("Event not found");
   }
 }); 
+
+app.post("/events", (req, res) => {
+  const newEvent : Event = req.body;
+  newEvent.id = events.length + 1;
+  events.push(newEvent);
+  res.json(newEvent);
+});
 
 app.listen(port, () => { 
   console.log((`App listening at http://localhost:${port}`))
